5f01782ae3a3fa98a0de25a9d7a0cef0489ec28a
Sergey Tereshchenko <serghh1988@gmail.com>
2021-05-10 22:37:35 +0300

    Добавил ссылки на кнопки переключения цветов. Прописал функции для кнопка запуска и остановки переключателя цветов.

--- js/index.js
+++ js/index.js
@@ -7,3 +7,32 @@ const colors = [
   '#795548',
 ];
 
+const INTERVAL_DELAY = 1000;
+
+const refs = {
+  bodyRef: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    colors.forEach(color => {
+      refs.bodyRef.style.backgroundColor = color;
+    });
+  }, INTERVAL_DELAY);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  console.log('остановили случайный подбор цвета');
+}
